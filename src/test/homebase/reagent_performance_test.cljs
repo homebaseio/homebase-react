772b6a83bbf4ca6d5c4f3902e49d3d6af00953e9
JB Rubinovitz <jrubinovitz@gmail.com>
2020-10-18 16:26:33 -0700

    add reagent stateful performance test

--- src/test/homebase/reagent_performance_test.cljs
+++ src/test/homebase/reagent_performance_test.cljs
@@ -9,22 +9,39 @@
 
 (def functional-compiler (r/create-compiler {:function-components true}))
 
-(defn test-functional []
+(def click-count (r/atom 0))
+
+(defn counting-component []
+  [:div
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:input {:type "button" :value "Click me!"
+            :on-click #(swap! click-count inc)}]])
+
+(defn test-functional [component]
   (js/performance.mark "functional-start")
   ; (simple-benchmark [x [hello-world-component]] (p/as-element functional-compiler x) 100000)
   (dotimes [i 100000]
-    (p/as-element functional-compiler [hello-world-component]))
+    (p/as-element functional-compiler [component]))
   (js/performance.mark "functional-end")
   (js/console.log (.stringify js/JSON  (js/performance.measure "functional" "functional-start" "functional-end"))))
 
-(defn test-class []
+(defn test-class [component]
   (js/performance.mark "class-start")
   ; (simple-benchmark [x [hello-world-component]] (p/as-element tmpl/default-compiler* x) 100000)
   (dotimes [i 100000]
-    (p/as-element tmpl/default-compiler* [hello-world-component]))
+    (p/as-element tmpl/default-compiler* [component]))
   (js/performance.mark "class-end")
   (js/console.log  (.stringify js/JSON  (js/performance.measure "class" "class-start" "class-end"))))
 
-(deftest create-element-test []
-  (test-functional)
-  (test-class))
+
+(deftest create-nonstateful-element-test []
+  (print "nonstateful test")
+  (test-functional hello-world-component)
+  (test-class hello-world-component))
+
+
+(deftest create-stateful-element-test []
+  (print "stateful test")
+  (test-functional counting-component)
+  (test-class counting-component))
