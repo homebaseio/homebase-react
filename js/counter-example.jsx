32d9d1804651df950a88b777411397505173fef9
JB Rubinovitz <jrubinovitz@gmail.com>
2020-10-30 14:16:06 -0700

    javascript benchmarking

--- js/counter-example.jsx
+++ js/counter-example.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-const { HomebaseProvider, useTransact, useEntity } = window.homebase.react
+// import { HomebaseProvider, useTransact, useQuery } from 'homebase-react'
+const { HomebaseProvider, useTransact, useQuery } = window.homebase.react
 
-const config = { 
-  initialData: [{
-    counter: {
-      identity: 'counter', 
-      count: 0
-    }
-  }]
-}
+const counterId = 1
+const config = { initialData: [{
+  ':db/id': counterId, 
+  ':count': 0 
+}]}
 
 export const App = () => (
   <HomebaseProvider config={config}>
@@ -17,17 +15,15 @@ export const App = () => (
 )
 
 const Counter = () => {
-  const [counter] = useEntity({ identity: 'counter' })
+  const [counter] = useQuery(counterId)
   const [transact] = useTransact()
   return (
     <div>
-      Count: {counter.get('count')}
+      Count: {counter.get(':count')}
       <div>
         <button onClick={() => transact([{
-          counter: {
-            id: counter.get('id'), 
-            count: counter.get('count') + 1
-          }
+          ':db/id': counterId, 
+          ':count': counter.get(':count') + 1
         }])}>
           Increment
         </button>
