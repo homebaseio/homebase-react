a4c7dfa90cd0a8240513a1edcb375bf1921e446f
JB Rubinovitz <jrubinovitz@gmail.com>
2020-11-23 17:50:48 -0800

    rename  'databaseClient' to 'homebaseClient' in index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,9 +5,9 @@ export type Entity = {
   get: (...params:string[]) => any;
 }
 export type Datom = [number, string, string | number | object | Array<any>, number, boolean];
-export type databaseClient = {
+export type homebaseClient = {
   dbToString: () => string,
-  dbFromString: (dbString: string) => databaseClient,
+  dbFromString: (dbString: string) => homebaseClient,
   dbToDatoms: () => Datom[],
   addTransactListener: (Facts: Datom[]) => void,
   removeTransactionListener: () => void,
@@ -18,4 +18,4 @@ export function HomebaseProvider(props: {config?:config, children:any}): any;
 export function useTransact(): [(transaction:Transaction) => any];
 export function useEntity(lookup: object | number): [Entity];
 export function useQuery(query: object | string, ...args: any): [Array<Entity>];
-export function useClient(): databaseClient;
+export function useClient(): homebaseClient;
