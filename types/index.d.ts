b1add71c7df236d0e4492c3e23462500a6a0c417
JB Rubinovitz <jrubinovitz@gmail.com>
2020-11-23 17:49:39 -0800

    rename Fact to Datom in index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,12 +4,12 @@ export type config = {schema?: Schema, initialData?: Transaction };
 export type Entity = {
   get: (...params:string[]) => any;
 }
-export type Fact = [number, string, string | number | object | Array<any>, number, boolean];
+export type Datom = [number, string, string | number | object | Array<any>, number, boolean];
 export type databaseClient = {
   dbToString: () => string,
   dbFromString: (dbString: string) => databaseClient,
-  dbToDatoms: () => Fact[],
-  addTransactListener: (Facts: Fact[]) => void,
+  dbToDatoms: () => Datom[],
+  addTransactListener: (Facts: Datom[]) => void,
   removeTransactionListener: () => void,
   transactSilently: (transaction: Transaction) => void
 }
