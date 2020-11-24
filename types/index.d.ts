a809a57eaf0db4d6b9acf70b77d8144aa4be915d
JB Rubinovitz <jrubinovitz@gmail.com>
2020-11-23 16:46:24 -0800

    feat(index.d.ts): add typescript type declaration for useClient react hook

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,8 +4,19 @@ export type config = {schema?: Schema, initialData?: Transaction };
 export type Entity = {
   get: (...params:string[]) => any;
 }
+export type Fact = [number, string, string | number | object | Array<any>, number, boolean];
+
+export type databaseClient = {
+  dbToString: () => string,
+  dbFromString: (dbString: string) => databaseClient,
+  dbToDatoms: () => Fact[],
+  addTransactListener: (Facts: Fact[]) => void,
+  removeTransactionListener: () => void,
+  transactSilently: (transaction: Transaction)=> void
+}
 
 export function HomebaseProvider(props: {config?:config, children:any}): any;
 export function useTransact(): [(transaction:Transaction) => any];
 export function useEntity(lookup: object | number): [Entity];
 export function useQuery(query: object | string, ...args: any): [Array<Entity>];
+export function useClient(): databaseClient;
