74a3c2bd7d75eb24cfe938805c82a924fc52b00c
rubinovitz <rubinovitz@users.noreply.github.com>
2020-11-24 15:44:30 -0800

    fix(types): add typescript type declaration for useClient react hook (#23)

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,8 +4,18 @@ export type config = {schema?: Schema, initialData?: Transaction };
 export type Entity = {
   get: (...params:string[]) => any;
 }
+export type Datom = [number, string, string | number | object | Array<any>, number, boolean];
+export type homebaseClient = {
+  dbToString: () => string,
+  dbFromString: (dbString: string) => any,
+  dbToDatoms: () => Datom[],
+  addTransactListener: (listener: (changedDatoms: Datom[]) => any) => any,
+  removeTransactListener: () => any,
+  transactSilently: (transaction: Transaction) => any
+}
 
 export function HomebaseProvider(props: {config?:config, children:any}): any;
 export function useTransact(): [(transaction:Transaction) => any];
 export function useEntity(lookup: object | number): [Entity];
 export function useQuery(query: object | string, ...args: any): [Array<Entity>];
+export function useClient(): homebaseClient;
