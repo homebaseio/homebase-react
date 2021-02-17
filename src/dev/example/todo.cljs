dcee1d920854a5b51bc6689253055b2e93a6abbd
Chris Smothers <chris.smothers@gmail.com>
2021-02-17 16:23:17 -0500

    test(react): add e2e tests (#63)

--- src/dev/example/todo.cljs
+++ src/dev/example/todo.cljs
@@ -1,8 +1,8 @@
-(ns example.todo
+(ns dev.example.todo
   (:require 
    [devcards.core :as dc]
    [homebase.react]
-   ["../js_gen/todo-example" :as react-example])
+   ["./js_compiled/todo" :as react-example])
   (:require-macros
    [devcards.core :refer [defcard-rg defcard-doc]]
    [dev.macros :refer [inline-resource]]))
@@ -12,11 +12,11 @@
 
 (def code-snippet 
   (clojure.string/replace-first
-   (inline-resource "js/todo-example.jsx")
+   (inline-resource "src/dev/example/js/todo.jsx")
    "const { HomebaseProvider, useTransact, useQuery, useEntity } = window.homebase.react"
    "import { HomebaseProvider, useTransact, useQuery, useEntity } from 'homebase-react'"))
 (defcard-doc
-  "[🔗GitHub](https://github.com/homebaseio/homebase-react/blob/master/js/todo-example.jsx)"
+  "[🔗GitHub](https://github.com/homebaseio/homebase-react/blob/master/src/dev/example/js/todo.jsx)"
   "For an annotated walkthrough of this code [check out the tutorial 📖](https://www.notion.so/Homebase-Alpha-Docs-0f0e22f3adcd4e9d87a13440ab0c7a0b)."
   (str "```javascript\n" code-snippet "\n```"))
 
